04cdd51bb8713fbec80abaaa4e11c8e0387288cc
mpcabete <mateus.cabete@usp.br>
2021-03-16 16:42:14 -0300

    gtag analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,20 @@ module.exports = {
       options: {
         gfm: true,
         plugins: [
+          {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                "G-JMVKZC1KP1", // Google Analytics / GA
+              ],
+              // This object is used for configuration specific to this plugin
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+              },
+            },
+          },
           netlifyCmsPaths,
           `gatsby-remark-reading-time`,
           {
@@ -70,12 +84,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-theme-ui',
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: settings.ga,
-      },
-    },
     `gatsby-plugin-advanced-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
